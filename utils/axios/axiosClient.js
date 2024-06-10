292533879ba0a2d5eda9c615128128f4473e6c4a
Sagar Adhikari <sagar.ingrails@gmail.com>
2024-06-10 10:58:13 +0545

    base url

--- utils/axios/axiosClient.js
+++ utils/axios/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+const baseURL = process.env.NEXT_PUBLIC_URL;
 // const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const axiosClient = axios.create({
   baseURL: baseURL,
